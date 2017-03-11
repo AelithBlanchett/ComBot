3ac3e49abeba29d59945af76f917b4c74f97076b
AelithBlanchett <tonyteur@yahoo.fr>
2017-03-11 13:23:58 -0500

    Delete LiveTester.ts

--- tests/LiveTester.ts
+++
@@ -1,3 +0,0 @@
-let FChatLib = require('fchatlib');
-let options = {username: "tonyteur", password: "aaaaaaaaaaaa", character: "Battle-fuck Arena", master: "Aelith Blanchette", room: "ADH-c7188539de793c27ddf8"};
-let myFchatBot = new FChatLib(options);
